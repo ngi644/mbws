8eca5fae9fca0e19fbcbc2bb506c95c54bdbfd8c
Takashi NAGAI <ngi644@gmail.com>
2021-03-04 00:13:19 +0900

    変数追加

--- main.ts
+++ main.ts
@@ -4,24 +4,26 @@ function ledOff () {
     pins.digitalWritePin(DigitalPin.P2, 0)
 }
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("A")
+    radio.sendString("B")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
-    if (receivedString == "A") {
+    if (receivedString == MyID) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P1, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(500)
-        radio.sendString("B")
+        radio.sendString("C")
     } else {
         ledOff()
     }
 })
+let MyID = ""
 ledOff()
 radio.setGroup(1)
+MyID = "A"
 basic.forever(function () {
 	
 })
